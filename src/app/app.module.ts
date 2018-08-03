f3d3c1b4886d15b0d8918c13f4d4e95d0d70899a
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2018-08-03 19:42:52 +0530

    phase-management changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,62 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavigationComponent } from './side-navigation/side-navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { 
-  MatToolbarModule, 
-  MatButtonModule, 
-  MatSidenavModule, 
-  MatIconModule, 
-  MatListModule, 
-  MatGridListModule, 
-  MatCardModule, 
-  MatMenuModule,
-  MatInputModule
-} from '@angular/material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { RoutingModule } from './/routing.module';
-import { PhaseManagementComponent } from './phase-management/phase-management.component';
-import { UnitManagementComponent } from './unit-management/unit-management.component';
-import { SheepManagementComponent } from './sheep-management/sheep-management.component';
-import { StaffManagementComponent } from './staff-management/staff-management.component';
-import { CustomersComponent } from './customers/customers.component';
-import { ExpencesComponent } from './expences/expences.component';
-import { SecurityManagementComponent } from './security-management/security-management.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    SideNavigationComponent,
-    DashboardComponent,
-    PhaseManagementComponent,
-    UnitManagementComponent,
-    SheepManagementComponent,
-    StaffManagementComponent,
-    CustomersComponent,
-    ExpencesComponent,
-    SecurityManagementComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SideNavigationComponent } from "./side-navigation/side-navigation.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+	MatToolbarModule,
+	MatButtonModule,
+	MatSidenavModule,
+	MatIconModule,
+	MatListModule,
+	MatGridListModule,
+	MatCardModule,
 	MatMenuModule,
 	MatInputModule,
-    RoutingModule,
-    NgxChartsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	MatTableModule,
+	MatPaginatorModule,
+	MatSortModule
+} from "@angular/material";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { RoutingModule } from ".//routing.module";
+import { PhaseManagementComponent } from "./phase-management/phase-management.component";
+import { UnitManagementComponent } from "./unit-management/unit-management.component";
+import { SheepManagementComponent } from "./sheep-management/sheep-management.component";
+import { StaffManagementComponent } from "./staff-management/staff-management.component";
+import { CustomersComponent } from "./customers/customers.component";
+import { ExpencesComponent } from "./expences/expences.component";
+import { SecurityManagementComponent } from "./security-management/security-management.component";
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		SideNavigationComponent,
+		DashboardComponent,
+		PhaseManagementComponent,
+		UnitManagementComponent,
+		SheepManagementComponent,
+		StaffManagementComponent,
+		CustomersComponent,
+		ExpencesComponent,
+		SecurityManagementComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		LayoutModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatSidenavModule,
+		MatIconModule,
+		MatListModule,
+		MatGridListModule,
+		MatCardModule,
+		MatMenuModule,
+		MatInputModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
+		RoutingModule,
+		NgxChartsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
