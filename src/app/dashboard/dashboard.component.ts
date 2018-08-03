15a66d90c9c60e598e73810a32c57ca4ef94f44e
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2018-08-03 15:03:29 +0530

    removing redundant data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -83,25 +83,6 @@ export class DashboardComponent {
 		}
 	];
 
-	  // guage data 
-
-	  guageData = [
-		{
-		  "name": "Germany",
-		  "value": 8940000
-		},
-		{
-		  "name": "USA",
-		  "value": 5000000
-		},
-		{
-		  "name": "France",
-		  "value": 7200000
-		}
-	  ];
-	
-	
-
 
 	onSelect(event) {
 		console.log(event);
